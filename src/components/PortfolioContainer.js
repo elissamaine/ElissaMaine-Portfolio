ceed4c1a48030ca9b09168c5756a4a3efb9d9677
elissamaine <emaine97@yahoo.com>
2023-06-12 17:30:24 -0700

    added and styled header

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -3,48 +3,48 @@ import './PortfolioContainer.scss';
 import React, { useState } from 'react';
 
 import Header from './Header';
-import Navbar from './Navbar';
-import Footer from './Footer';
+// import Navbar from './Navbar';
+// import Footer from './Footer';
 
-import Home from './pages/Home';
-import About from './pages/About';
-import Portfolio from './pages/Portfolio';
-import Resume from './pages/Resume';
-import Contact from './pages/Contact';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import Portfolio from './pages/Portfolio';
+// import Resume from './pages/Resume';
+// import Contact from './pages/Contact';
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('Home');
+  // const [currentPage, setCurrentPage] = useState('Home');
   
-  const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
-    }
-    if (currentPage === 'About') {
-      return <About />;
-    } 
-    if (currentPage === 'Portfolio') {
-      return <Portfolio />;
-    }
-    if (currentPage === 'Resume') {
-      return <Resume />;
-    }
-    if (currentPage === 'Contact') {
-      return <Contact />;
-    }
-  };
+  // const renderPage = () => {
+  //   if (currentPage === 'Home') {
+  //     return <Home />;
+  //   }
+  //   if (currentPage === 'About') {
+  //     return <About />;
+  //   } 
+  //   if (currentPage === 'Portfolio') {
+  //     return <Portfolio />;
+  //   }
+  //   if (currentPage === 'Resume') {
+  //     return <Resume />;
+  //   }
+  //   if (currentPage === 'Contact') {
+  //     return <Contact />;
+  //   }
+  // };
   
-  const handlePageChange = (page) => setCurrentPage(page);
+  // const handlePageChange = (page) => setCurrentPage(page);
   
   return (
     <div>
       <Header />
       
       
-      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* <Navbar currentPage={currentPage} handlePageChange={handlePageChange} /> */}
       
-      {renderPage()}
+      {/* {renderPage()} */}
       
-      <Footer />
+      {/* <Footer /> */}
       <h1>heloo</h1>
       
     </div>
