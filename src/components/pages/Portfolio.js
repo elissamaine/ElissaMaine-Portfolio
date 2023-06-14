3644b6dcc492f2fd2f897030c09ea2fd96da1413
elissamaine <emaine97@yahoo.com>
2023-06-13 18:12:46 -0700

    added basic layout to portfolio page

---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,18 @@
+import './Portfolio.scss';
+
+import React from 'react';
+
+const Portfolio = () => {
+  return (
+    <div className='portfolio-page'>
+      <div className='title'>
+        <h1>Portfolio</h1>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio;
