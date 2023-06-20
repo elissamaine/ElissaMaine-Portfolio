02380fb2ce02059b17a5e56762b88ed6ef463b29
elissamaine <emaine97@yahoo.com>
2023-06-20 15:53:07 -0700

    added info to portfolio

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,53 +1,43 @@
 import './Project.scss'
 
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faDesktop } from '@fortawesome/free-solid-svg-icons';
 
-import proj1 from '../images/proj1.PNG';
-import proj2 from '../images/proj2.PNG';
-import proj3 from '../images/proj3.PNG';
-import proj4 from '../images/proj4.PNG';
+// import proj1 from '../images/proj1.PNG';
+// import proj2 from '../images/proj2.PNG';
+// import proj3 from '../images/proj3.PNG';
+// import proj4 from '../images/proj4.PNG';
 
-const Project = () => {
-  
-  const projects = [
-    {
-      title: 'Story Finder',
-      description: 'A site for resders to find and save books',
-      image: proj1,
-      github: 'https://github.com/b-crites/BookClub',
-      deployed: 'https://b-crites.github.io/BookClub/'
-    },
-    {
-      title: 'Password Generator',
-      description: 'A simple pasword generator that allows the user to select the length and type of characters to include in their password',
-      image: proj2,
-      github: 'https://github.com/elissamaine/Password-Generator',
-      deployed: 'https://elissamaine.github.io/Password-Generator/'
-    },
-    {
-      title: `Cook's Companion`,
-      description: 'A site for users to save and share their favorite recipes',
-      image: proj3,
-      github: 'https://github.com/andrewmuhn/cooks-companion',
-      deployed: 'https://cookscompanion.herokuapp.com/'
-    },
-    {
-      title: 'Tech Blog',
-      description: 'A site for users to share their thoughts and opinions on tech',
-      image: proj4,
-      github: 'https://github.com/elissamaine/Tech_Blog',
-      deployed: 'https://aqueous-dusk-47864.herokuapp.com/'
-    }
-  ]
+function Project(props) {
 
   return (
-    <div className='project'>
-
-
-      <FontAwesomeIcon icon={faGithub} />
-      <FontAwesomeIcon icon={faDesktop} />
+    <div className='project-contaioner'>
+      
+      {props.projects.map(project => (
+        <div className='project'>
+          <div className='project-img'>
+            <img src={project.image} alt={project.title} />
+          </div>
+          <div className='project-info'>
+            <h3>{project.title}</h3>
+            <p>{project.description}</p>
+            <div className='links'>
+              <a className='repo-link' href={project.github}>
+                <span>
+                  <FontAwesomeIcon icon={faGithub} />
+                </span>
+              </a>
+              <a className='deploy-link' href={project.deployed}>
+                <span>
+                  <FontAwesomeIcon icon={faDesktop} />
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   )
 };
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,8 +1,43 @@
 import './Portfolio.scss';
 
 import Project from '../Project.js';
+import proj1 from '../../images/proj1.PNG';
+import proj2 from '../../images/proj2.PNG';
+import proj3 from '../../images/proj3.PNG';
+import proj4 from '../../images/proj4.PNG';
 
 import React from 'react';
+
+const projects = [
+  {
+    title: 'Story Finder',
+    description: 'A site for resders to find and save books',
+    image: proj1,
+    github: 'https://github.com/b-crites/BookClub',
+    deployed: 'https://b-crites.github.io/BookClub/'
+  },
+  {
+    title: 'Password Generator',
+    description: 'A simple pasword generator that allows the user to select the length and type of characters to include in their password',
+    image: proj2,
+    github: 'https://github.com/elissamaine/Password-Generator',
+    deployed: 'https://elissamaine.github.io/Password-Generator/'
+  },
+  {
+    title: `Cook's Companion`,
+    description: 'A site for users to save and share their favorite recipes',
+    image: proj3,
+    github: 'https://github.com/andrewmuhn/cooks-companion',
+    deployed: 'https://cookscompanion.herokuapp.com/'
+  },
+  {
+    title: 'Tech Blog',
+    description: 'A site for users to share their thoughts and opinions on tech',
+    image: proj4,
+    github: 'https://github.com/elissamaine/Tech_Blog',
+    deployed: 'https://aqueous-dusk-47864.herokuapp.com/'
+  }
+]
 
 const Portfolio = () => {
   return (
@@ -11,7 +46,7 @@ const Portfolio = () => {
         <h1>Portfolio</h1>
       </div>
       <div className='project-container'>
-        <Project />
+        <Project projects={projects} />
       </div>
     </main>
   );
