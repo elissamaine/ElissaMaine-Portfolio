b75039e7601348ea1ac6b1aa25ea7aef0dfde6ec
elissamaine <emaine97@yahoo.com>
2023-06-29 16:51:22 -0700

    added about me information

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -28,16 +28,15 @@ export default function About() {
           />
         </h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-          velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-          ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-          non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-          ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-          rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-          tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-          porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-          vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-          fames ac ante ipsum primis in faucibus. 
+        ‧˚✩ Hello ✩˚‧
+        <br/>
+        My name is Elissa Maine and am a recent full stack coding bootcamp graduate looking to start my career. 
+        <br/>
+        <br/>
+        I originally got my degree in Geology from San Diego State University, but after moving to Vancouver Washington I decided to change career paths and took a coding bootcamp. I have become passionate about designing and creating projects and I enjoy challenging myself to learn new skills.
+        <br/>
+        <br/>
+        I am proficient in front end and back end development but I’ve grown to favor front end development and figuring out how to make a site more interactive and enjoyable for the user. 
         </p>
       </div>
       <div className='image div'>
